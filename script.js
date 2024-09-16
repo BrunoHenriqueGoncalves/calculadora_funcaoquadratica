00b1353683fc695a6a337339d98772298bc0b8da
BrunoHenriqueGoncalves <159266799+BrunoHenriqueGoncalves@users.noreply.github.com>
2024-09-16 09:34:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-let a = parseFloat(document.getElementById('a').value);
-let b = parseFloat(document.getElementById('b').value);
-let c = parseFloat(document.getElementById('c').value);
-let resultado = 0;
-    if(isNaN(num1) || isNaN(num2)){
-        alert('fala um número idiota do caralho :D');
-    }
-let delta= (b*b) * (-4 * a * c);
-delta= Math.sqrt(delta);
-let x_1= (-b + delta)/(2*a);
-let x_2= (-b - delta)/(2*a);
+
+function calcula() {
+    const a = parseFloat(document.getElementById('a').value);
+    const b = parseFloat(document.getElementById('b').value);
+    const c = parseFloat(document.getElementById('c').value);
+    let delta= (b*b) - (4 * a * c);
+    let rdelta= Math.sqrt(delta);
+
+    if(delta == 0){
+    alert('não há números reais');
+}else if(delta < 0){
+    let x1= (-b)/(2*a);
+    document.getElementById('res').innerText = 'Resultado: ' + x1;
+}else{
+    let x1= (-b+rdelta)/(2*a);
+    let x2= (-b-rdelta)/(2*a);
+    document.getElementById('res').innerText = 'Resultado: ' + x1 + ' e ' + x2;
+}
+}
