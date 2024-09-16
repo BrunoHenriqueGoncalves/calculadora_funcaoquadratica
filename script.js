183740ed638863653c009250312321e65acd349f
BrunoHenriqueGoncalves <159266799+BrunoHenriqueGoncalves@users.noreply.github.com>
2024-09-16 09:37:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function calcula() {
     let rdelta= Math.sqrt(delta);
 
     if(delta == 0){
-    alert('não há números reais');
-}else if(delta < 0){
-    let x1= (-b)/(2*a);
-    document.getElementById('res').innerText = 'Resultado: ' + x1;
-}else{
-    let x1= (-b+rdelta)/(2*a);
-    let x2= (-b-rdelta)/(2*a);
-    document.getElementById('res').innerText = 'Resultado: ' + x1 + ' e ' + x2;
-}
+        alert('não há números reais');
+    }else if(delta < 0){
+        let x1= (-b)/(2*a);
+        document.getElementById('res').innerText = x1;
+    }else{
+        let x1= (-b+rdelta)/(2*a);
+        let x2= (-b-rdelta)/(2*a);
+        document.getElementById('res').innerText = x1 + ' e ' + x2;
+    }
 }
